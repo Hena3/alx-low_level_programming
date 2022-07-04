c611947a72decde31908ae5cfa12e72f4f55dabe
henok <henok@example.com>
2022-07-04 14:43:13 -0700

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,15 +5,10 @@
  */
 int main(void)
 {
-int a;
-long int b;
-long long int c;
-char c;
-float f;
-printf("size of a char : %lu byte(s)\n", (unsigned long)size of (d));
-printf("size of an int : %lu byte(s)\n", (unsigned long)size of (a)); 
-printf("size of a long int : %lu byte(s)\n", (unsigned long)size of (b)); 
-printf("size of a long long int : %lu byte(s)\n", (unsigned long)size of (c)); 
-printf("size of a float : %lu byte(s)\n", (unsigned long)size of (f));
+printf("size of a char: %lu byte(s)\n", size of(char));
+printf("size of an int: %lu byte(s)\n", size of(int)); 
+printf("size of a long int: %lu byte(s)\n", size of(long int)); 
+printf("size of a long long int: %lu byte(s)\n", size of(long long int)); 
+printf("size of a float: %lu byte(s)\n", size of(float));
 return (0);
 }
