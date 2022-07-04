6f62f52ab2c7b2126d6798959dfc2053374e51a5
henok <henok@example.com>
2022-07-04 10:34:47 -0700

    My first commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - starting point
+ * Return:alwas return 0
+ */
+int main(void)
+{
+char a;
+for (a == 'a'; a <= 'z'; a++)
+{
+if (a == 'q' || a == 'e')
+{
+continue ;
+}
+putchar (a);
+return (0);
+}
