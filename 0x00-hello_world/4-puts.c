8b6ca277ad91a5fddf3305bdcc6ece73c50bb5ef
henok <henok@example.com>
2022-07-04 14:23:16 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,7 @@
  * Return: 0 after printing the function
  */
 int main(void)
-{puts("\"programing is like building a multilingual puzzle");
+{
+puts("\"programing is like building a multilingual puzzle");
 return (0);
 }
