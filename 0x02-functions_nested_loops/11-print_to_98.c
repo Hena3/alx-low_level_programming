b1c3bc6114462d1baeeb1d66b7c9d162233c0a46
henok <henok@example.com>
2022-07-06 15:44:23 -0700

    commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,7 +6,7 @@
  */
 void print_to_98(int n)
 {
-if (n <=98)
+if (n <= 98)
 {
 while (n <= 98)
 {
@@ -25,7 +25,7 @@ if (n == 98)
 printf("%d\n", n);
 else
 printf("%d, ", n);
-n = n -1;
+n = n - 1;
 }
 }
 }
