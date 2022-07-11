51e875adb791f2d30d90003eb88e0fb320599fe7
henok <henok@example.com>
2022-07-11 13:14:47 -0700

    commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,10 +4,11 @@
  * print_array -> prints n elements of an array of integers
  * @a: pointer to array
  * @n: number of elements
+ * Return: void
  */
 void print_array(int *a, int n)
 {
-int i:
+int i;
 for (i = 0; i < n; i++)
 {
 printf("%d", a[i]);
