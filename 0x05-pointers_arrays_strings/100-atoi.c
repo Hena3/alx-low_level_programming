66d2f228368272b324f0fdaa5aceb55b30ba34ea
henok <henok@example.com>
2022-07-12 09:30:22 -0700

    commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,22 +8,22 @@ int _atoi(char *s)
 {
 int sign;
 unsigned int num;
-char *tmp;
-tmp = s;
+char *temp;
+temp = s;
 num = 0;
 sign = 1;
-while (*tmp != '\0' && (*tmp < '0' || *tmp > '9'))
+while (*temp != '\0' && (*temp < '0' || *temp > '9'))
 {
-if (*tmp == '-')
+if (*temp == '-')
 sign *= -1;
-tmp++;
+temp++;
 }
-if (*tmp != '\0')
+if (*temp != '\0')
 {
 do {
-num = num * 10 + (*tmp - '0');
-tmp++;
-} while (*tmp >= '0' && *tmp <= '9');
+num = num * 10 + (*temp - '0');
+temp++;
+} while (*temp >= '0' && *temp <= '9');
 }
 return (num * sign);
 }
