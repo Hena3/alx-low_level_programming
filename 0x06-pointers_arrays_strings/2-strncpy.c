d94e7cfc5ad84147c2978392a49de24cc5bf8653
henok <henok@example.com>
2022-07-14 08:44:46 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include"main.h"
+#include<string.h>
+/**
+ * strncpy - for copying purpose
+ * @src: param1
+ * @dest: param2
+ * @n: pram3
+ * Return: string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+strncpy(dest, src, n);
+return (dest);
+}
