47519b62f594e21d632c820f372a140cf7ae0cfb
henok <henok@example.com>
2022-07-14 03:59:15 -0700

    commit

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,9 +1,9 @@
-#include <unistd.h>
+#include<unistd.h>
 /**
  * _putchar - writes the character c to stdout
  * @c the character to print
- * Return: On success 1.
- * On error, -1 is returned, and erron is not appropriately.
+ * Return: On success 1
+ * On error, -1 is returned, and erron is not appropriately
  */
 int _putchar(char c)
 {
