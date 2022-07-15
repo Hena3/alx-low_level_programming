e56f0b86fdacc071f707676b063b262277de6935
henok <henok@example.com>
2022-07-15 14:09:01 -0700

    commit

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -27,6 +27,12 @@ else
 putchar('.');
 }
 }
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ * Return: void
+ */
 void print_buffer(char *b, int size)
 {
 int i;
