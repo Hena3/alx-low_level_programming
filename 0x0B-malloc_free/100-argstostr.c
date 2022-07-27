27337f84895b74ec0c3573f715c956f25500bfba
henok <henok@example.com>
2022-07-27 14:24:33 -0700

    commit

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -30,4 +30,20 @@ i++;
 arg = malloc((sizeof(char) * size) +1);
 if (arg == NULL)
 return (NULL);
+i = 0;
+while (i < ac)
+{
+j = 0;
+while (av[i][j])
+{
+arg[k] = av[i][j];
+j++;
+k++;
+}
+arg[k] = '\n';
+k++;
+i++;
+}
+arg[k] = '\0';
+return (arg);
 }
