72d170672f466112721992a75f9a77abf81c7401
henok <henok@example.com>
2022-08-05 09:48:15 -0700

    commit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,6 +12,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *s;
 	va_list strings;
+
 	if (separator == NULL)
 		separator = "";
 	va_start(strings, n);
