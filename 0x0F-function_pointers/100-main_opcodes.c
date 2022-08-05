90315e32397970f3705f347bc6ff779fbfb2251b
henok <henok@example.com>
2022-08-05 09:16:16 -0700

    commit

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 		exit(2);
 	}
 
-	for (count = 0; count <= bytes; count++)
+	for (count = 0; count < bytes; count++)
 	{
 printf("%02hhx", *((char *)main + count));
 if (count  < bytes - 1)
